859e9eed596571efe6ad6c220d3358e8d445015c
Akshat Dubey <k22013945@kcl.ac.uk>
2023-12-06 06:25:59 +0000

    changes

--- employee.py
+++ employee.py
@@ -2,46 +2,46 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 class Employee:
-    def __init__(self, name, contract_type, salary=0, hours_worked=1, hourly_rate=0, bonus=0, contracts=1, commission_rate=0):
+    def __init__(self, name, type, salary=0, hours=1, rate=0, bonus=0, contracts=1, commission=0):
         self.name = name
-        self.contract_type = contract_type
+        self.type = type
         self.salary = salary
-        self.hours_worked = hours_worked
-        self.hourly_rate = hourly_rate
+        self.hours = hours
+        self.rate = rate
         self.bonus = bonus
         self.contracts = contracts
-        self.commission_rate = commission_rate
+        self.commission = commission
 
     def get_pay(self):
-        if self.contract_type == 'salary':
+        if self.type == 'salary':
             contract_pay = self.salary
-        elif self.contract_type == 'hourly':
-            contract_pay = self.hours_worked * self.hourly_rate
+        elif self.type == 'hourly':
+            contract_pay = self.hours * self.rate
         else:
             raise ValueError("Invalid contract type")
 
         commission = 0
         if self.bonus:
             commission += self.bonus
-        if self.contracts and self.commission_rate:
-            commission += self.contracts * self.commission_rate
+        if self.contracts and self.commission:
+            commission += self.contracts * self.commission
 
         total_pay = contract_pay + commission
         return total_pay
 
     def __str__(self):
-        if self.contract_type == 'salary':
+        if self.type == 'salary':
             explanation = f"{self.name} works on a monthly salary of {self.salary}"
-        elif self.contract_type == 'hourly':
-            explanation = f"{self.name} works on a contract of {self.hours_worked} hours at {self.hourly_rate}/hour"
+        elif self.type == 'hourly':
+            explanation = f"{self.name} works on a contract of {self.hours} hours at {self.rate}/hour"
         else:
             raise ValueError("Invalid contract type")
 
         commission_explanation = ""
         if self.bonus!=0:
             commission_explanation += f" and receives a bonus commission of {self.bonus}"
-        if self.contracts and self.commission_rate:
-            commission_explanation += f" and receives a commission for {self.contracts} contract(s) at {self.commission_rate}/contract"
+        if self.contracts and self.commission:
+            commission_explanation += f" and receives a commission for {self.contracts} contract(s) at {self.commission}/contract"
 
         total_explanation = f". Their total pay is {self.get_pay()}."
 
@@ -51,19 +51,19 @@ class Employee:
 billie = Employee('Billie', 'salary', salary=4000)
 
 # Charlie
-charlie = Employee('Charlie', 'hourly', hours_worked=100, hourly_rate=25)
+charlie = Employee('Charlie', 'hourly', hours=100, rate=25)
 
 # Renee
-renee = Employee('Renee', 'salary', salary=3000, contracts=4, commission_rate=200)
+renee = Employee('Renee', 'salary', salary=3000, contracts=4, commission=200)
 
 # Jan
-jan = Employee('Jan', 'hourly', hours_worked=150, hourly_rate=25, contracts=3, commission_rate=220)
+jan = Employee('Jan', 'hourly', hours=150, rate=25, contracts=3, commission=220)
 
 # Robbie
 robbie = Employee('Robbie', 'salary', salary=2000, bonus=1500)
 
 # Ariel
-ariel = Employee('Ariel', 'hourly', hours_worked=120, hourly_rate=30, bonus=600)
+ariel = Employee('Ariel', 'hourly', hours=120, rate=30, bonus=600)
 
 print(billie.get_pay())  # Output: 4000
 print(str(billie))  # Output: "Billie works on a monthly salary of 4000. Their total pay is 4000."
